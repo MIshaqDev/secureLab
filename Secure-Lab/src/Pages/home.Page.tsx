37743f6c191f8cec5be8b9d251f97fe125bcd476
Muhammad Ishaq <muhammadishaq.dev@gmail.com>
2026-02-01 13:41:37 +0500

    Update home.Page.tsx

--- Secure-Lab/src/Pages/home.Page.tsx
+++ Secure-Lab/src/Pages/home.Page.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/Logo.png";
 
 function HomePage() {
     return (
-        <div className="flex flex-col items-center bg-[var(--background-color)] w-screen h-screen gap-5 pt-10">
+        <div className="flex flex-col items-center bg-[var(--background-color)] w-screen min-h-screen gap-5 pt-10">
             <img src={logo} alt="" className="w-30 rounded rounded-full hover:opacity-75" />
             <h1 className="text-[var(--text-color)] text-4xl font-bold font-[Space Mono] text-center md:text-4xl">Explore the World of Ciphers</h1>
             <p className="text-[var(--text-color)] m-10 text-2xl font-justify font-[Space Mono] text-center">Welcome to Secure Lab — your interactive cryptography playground. Experiment with classic ciphers like Caesar, Monoalphabetic, Vigenère, and Row Transposition, or explore our modern custom encryption. Enter your text, choose a key, and see your messages transform in real time. Secure Lab is designed to make learning encryption intuitive, hands-on, and fun, whether you’re a student, developer, or just curious about the world of secure communication. Dive in, encrypt your secrets, and discover the art of cryptography!</p>
